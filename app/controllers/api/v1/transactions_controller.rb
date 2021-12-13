bdbb73c781548d6ba7d64975569d6a4fac204fee
Ignacio Madariaga <inmadariaga@uc.cl>
2021-12-13 19:05:23 -0300

    decoded

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -16,6 +16,11 @@ class Api::V1::TransactionsController < Api::V1::BaseController
     puts decode(transactions_params["data"])
     puts transactions_params["messageId"]
     puts transactions_params["message_id"]
+    if decode(transactions_params["data"]).length == 64
+      puts elements(decode(transactions_params["data"]))
+    else
+      pute "NO"
+    end
     /respond_with Transaction.create!(transactions_params)/
   end
 
@@ -39,5 +44,16 @@ class Api::V1::TransactionsController < Api::V1::BaseController
 
   def decode(data)
     return Base64.decode64(data)
+  end
+
+  def elements(decoded_data)
+    operation = decoded_data[0..3].to_i
+    id = decoded_data[4..13].to_i
+    origin_bank = decoded_data[14..20].to_i
+    origin_account = decoded_data[21..30].to_i
+    destination_bank = decoded_data[31..37].to_i
+    destination_account = decoded_data[38..47].to_i
+    amount = decoded_data[48..63].to_i
+    return { operation: operation, id: id, origin_bank: origin_bank, origin_account: origin_account, destination_bank: destination_bank, destination_account: destination_account, amount: amount }
   end
 end
